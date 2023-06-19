42f166ce4438107c84b29cf0e245735f366d5864
min02choi <min02choi@naver.com>
2023-06-19 23:28:38 +0900

    [PC:2023-06-19] Create View

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+# url에 걸리면 view를 보여줌
 urlpatterns = [
+    # ex: /polls/
     path("", views.index, name="index"),
+    # ex: /polls/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
-# Create your views here.
+# view: 클라이언트에게서 request를 받고 HttpResponse 메소드로 반환을 해주는 구조
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world!!!")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." %question_id)
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." %question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." %question_id)
