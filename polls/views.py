e3aad0eb400b7ea30edd122dfff25a968a47cabb
min02choi <min02choi@naver.com>
2023-08-23 22:41:02 +0900

    [Part4] Form, Generic View

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,33 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from django.template import loader
-from django.shortcuts import render
 
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
-from .models import Question
+from django.views import generic
+
+from django.urls import reverse
+from .models import Question, Choice
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 # view: 클라이언트에게서 request를 받고 HttpResponse 메소드로 반환을 해주는 구조
 def index(request):
@@ -35,8 +55,28 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
-    return HttpResponse("You're looking at the results of question %s." %question_id)
-    
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    # 데이터가 없는 경우
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render (
+            request,
+            "polls/detail.html", {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        # HttpResponseRedirect는 post와 같이 쓰인다고 보면 됨
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
