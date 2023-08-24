f2d94bc4e54ad034c8868d87f006c50aeb642deb
min02choi <min02choi@naver.com>
2023-08-24 18:08:45 +0900

    [Part4] Form, Generic View

--- polls/urls.py
+++ polls/urls.py
@@ -6,10 +6,8 @@ app_name = "polls"
 # url에 걸리면 view를 보여줌(views.py에서 해당하는 view로 연결)
 # path 안의 url은 장고에서 지원해주는 패턴
 urlpatterns = [
-    # ex: /polls/
-    path("", views.index, name="index"),
-    # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
